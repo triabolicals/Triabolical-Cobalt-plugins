21b919bc2c7f2e6e016feaec63505751362f1ab9
triabolicals <138998174+triabolicals@users.noreply.github.com>
2024-01-30 16:56:59 -0500

    Update NoCaps.rs to account for added jobs/perons

--- src/NoCaps.rs
+++ src/NoCaps.rs
@@ -7,7 +7,6 @@ use skyline::patching::Patch;
 use cobapi::Event;
 use cobapi::SystemEvent;
 
-
 pub const BUFF_POS: &[usize] = &[
     0x01f7640c, 0x01f7643c, 0x01f76470, 0x01f764a4, 0x01f764d8, 0x01f76510, 0x01f76548, 0x01f7657C, 
     0x01f765b0, 0x01f765e8, 0x01f76620, 0x01f77b28, 0x01f77b58, 0x01f77b8c, 0x01f77bc0, 0x01f77bf4, 
@@ -15,6 +14,7 @@ pub const BUFF_POS: &[usize] = &[
     0x01a1d838, 0x01a1d86c, 0x01a1d8a0, 0x01a1d8d8, 0x01a1d910, 0x01a1d944, 0x01a1d978, 0x01a1d9b0, 
     0x01a1d9e8 
 ];
+
 
 #[unity::from_offset("App", "JobData", "get_Limit")]
 pub fn job_get_limit(this: &JobData, method_info: OptionalMethod) -> & mut Capability;
@@ -31,17 +31,21 @@ pub fn set_job_caps(){
         let t_list = &triabolical.list.items;
         let triabolical2 = JobData::get_list_mut().expect("triabolical2 is 'None'");
         let t_list2 = &triabolical2.list.items;
-        for x in 0..1522 {
+        let job_count: usize = JobData::get_count() as usize;
+        let person_count: usize = PersonData::get_count() as usize;
+        for x in 0..person_count {
             //Setting personal caps to 0
             let caps = get_limit(t_list[x], None);
             for i in 0..11 { caps.array.m_item[i] = 0; }
         }
-        for x in 0..111 {
+        for x in 0..job_count {
             //Setting job caps to 127 + Base
             let job = &t_list2[x];
             let cap = job_get_limit(job, None);
             let base = job_get_base(job, None);
-            for i in 0..10 { cap.array.m_item[i] = 127 + base.array.m_item[i]; }
+            for i in 0..10 { 
+                cap.array.m_item[i] = 127 + base.array.m_item[i];
+             }
             //Move Cap is set to 99
             cap.array.m_item[10] = 99;
             job_set_limit(job, cap, None);
@@ -61,13 +65,16 @@ extern "C" fn do_job_caps(event: &Event<SystemEvent>) {
     } 
     else {  println!("We received a missing event, and we don't care!"); }
 }
-
+pub fn return999(offset: usize){
+    Patch::in_text(offset).bytes(&[0xE0, 0x7C, 0x80, 0x52]);
+    Patch::in_text(offset+0x4).bytes(&[0xC0, 0x03, 0x5F, 0xD6]);
+}
 #[skyline::main(name = "Max Stat Caps")]
 pub fn main() {
     cobapi::register_system_event_handler(do_job_caps);
     Patch::in_text(0x01a2a7c0).bytes(&[0xe1,0x0e,0x80,0x12]);
     Patch::in_text(0x01a2a7c4).bytes(&[0x02,0x0f,0x80,0x52]);
-    let postive99 = &[0xE2, 0x1F, 0x80, 0x52];
+    let postive99 = &[0xE2, 0x7C, 0x80, 0x52];
     let negative99 = &[0x61, 0x0C, 0x80, 0x12];
     // Enhance limit change to -99 to 99
     for x in BUFF_POS {
