77506c4900b396d2b2d6a56b1dfeb0d108c43637
triabolicals <138998174+triabolicals@users.noreply.github.com>
2024-02-15 16:22:55 -0500

    Update NoCaps.rs

--- src/NoCaps.rs
+++ src/NoCaps.rs
@@ -1,6 +1,7 @@
 #![feature(lazy_cell, ptr_sub_ptr)]
 use unity::prelude::*;
 use unity::{il2cpp::class::Il2CppRGCTXData, prelude::*};
+use unity::il2cpp::object::Array;
 use engage::gamedata::{*, person::*, unit::*};
 use engage::{sequence::*, gamevariable::*, gameuserdata::*};
 use skyline::patching::Patch;
@@ -8,13 +9,22 @@ use cobapi::Event;
 use cobapi::SystemEvent;
 
 pub const BUFF_POS: &[usize] = &[
-    0x01f7640c, 0x01f7643c, 0x01f76470, 0x01f764a4, 0x01f764d8, 0x01f76510, 0x01f76548, 0x01f7657C, 
-    0x01f765b0, 0x01f765e8, 0x01f76620, 0x01f77b28, 0x01f77b58, 0x01f77b8c, 0x01f77bc0, 0x01f77bf4, 
-    0x01f77c2c, 0x01f77c64, 0x01f77c98, 0x01f77ccc, 0x01f77d04, 0x01f77d3c, 0x01a1d7d4, 0x01a1d804, 
-    0x01a1d838, 0x01a1d86c, 0x01a1d8a0, 0x01a1d8d8, 0x01a1d910, 0x01a1d944, 0x01a1d978, 0x01a1d9b0, 
-    0x01a1d9e8 
+    0x01a1c92c,    0x01a1c95c,     0x01a1c990,    0x01a1c9c4, 
+    0x01a1c9f8,    0x01a1ca30,     0x01a1ca68,    0x01a1ca9c, 
+    0x01a1cad0,    0x01a1cb08,     0x01a1cb40,    0x01a1d7d0, 
+    0x01a1d800,    0x01a1d834,     0x01a1d868,    0x01a1d89c, 
+    0x01a1d8d4,    0x01a1d90c,     0x01a1d940,    0x01a1d974, 
+    0x01a1d9ac,    0x01a1d9e4,     0x01f76408,    0x01f76438, 
+    0x01f7646c,    0x01f764a0,     0x01f764d4,    0x01f7650c, 
+    0x01f76544,    0x01f76578,     0x01f765ac,    0x01f765e4, 
+    0x01f7661c,    0x01f77b24,     0x01f77b54,    0x01f77b88, 
+    0x01f77bbc,    0x01f77bf0,     0x01f77c28,    0x01f77c60, 
+    0x01f77c94,    0x01f77cc8,     0x01f77d00,    0x01f77d38, 
 ];
-
+#[unity::class("App","Capability")]
+pub struct Capability {
+    pub m_Data: &'static mut Array<u8>,
+}
 
 #[unity::from_offset("App", "JobData", "get_Limit")]
 pub fn job_get_limit(this: &JobData, method_info: OptionalMethod) -> & mut Capability;
@@ -36,7 +46,7 @@ pub fn set_job_caps(){
         for x in 0..person_count {
             //Setting personal caps to 0
             let caps = get_limit(t_list[x], None);
-            for i in 0..11 { caps.array.m_item[i] = 0; }
+            for i in 0..11 { caps.m_Data[i] = 0; }
         }
         for x in 0..job_count {
             //Setting job caps to 127 + Base
@@ -44,10 +54,10 @@ pub fn set_job_caps(){
             let cap = job_get_limit(job, None);
             let base = job_get_base(job, None);
             for i in 0..10 { 
-                cap.array.m_item[i] = 127 + base.array.m_item[i];
+                cap.m_Data[i] = 127 + base.m_Data[i];
              }
             //Move Cap is set to 99
-            cap.array.m_item[10] = 99;
+            cap.m_Data[10] = 99;
             job_set_limit(job, cap, None);
         }
     }
@@ -64,10 +74,6 @@ extern "C" fn do_job_caps(event: &Event<SystemEvent>) {
         }
     } 
     else {  println!("We received a missing event, and we don't care!"); }
-}
-pub fn return999(offset: usize){
-    Patch::in_text(offset).bytes(&[0xE0, 0x7C, 0x80, 0x52]);
-    Patch::in_text(offset+0x4).bytes(&[0xC0, 0x03, 0x5F, 0xD6]);
 }
 #[skyline::main(name = "Max Stat Caps")]
 pub fn main() {
